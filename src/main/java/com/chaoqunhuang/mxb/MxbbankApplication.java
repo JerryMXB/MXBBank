aba062e84ecb537cbc3df0c3c799329e2aa4544f
Chaoqun Huang <jerryhuangchaoqun@gmail.com>
2018-04-23 18:26:47 -0400

    init repo

---
+++ src/main/java/com/chaoqunhuang/mxb/MxbbankApplication.java
@@ -0,0 +1,12 @@
+package com.chaoqunhuang.mxb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxbbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MxbbankApplication.class, args);
+	}
+}
